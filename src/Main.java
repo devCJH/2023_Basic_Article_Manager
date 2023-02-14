a627052c93324f2b2b0ce911331fa92eb92a5bcd
devCJH <http-@naver.com>
2023-02-14 15:30:26 +0900

    게시글 작성 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,35 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int lastArticleId = 0;
+		
 		while(true) {
+			
+			
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
 			
+			if(cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+			
 			if(cmd.equals("exit")) {
 				break;
+			}else if(cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다\n", id);
+				
+			}else if(cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			}else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 			
 		}
