ebc9c76cced4a83f9dc082b4124759d61b293526
devCJH <http-@naver.com>
2023-02-14 14:00:05 +0900

    github 업로드 연습

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String cmd = sc.nextLine();
+		
+		System.out.println("입력된 명령어 : " + cmd);
+		
+		System.out.println("== 프로그램 끝 ==");
+		
+		sc.close();
+		
+	}
+}
