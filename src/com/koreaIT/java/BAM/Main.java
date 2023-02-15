15b01420834bd14b98f242555845127d9e4d071e
devCJH <http-@naver.com>
2023-02-15 13:49:02 +0900

    게시물 리스트 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -35,15 +35,27 @@ public class Main {
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
+				
+				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다\n", id);
 
 			} else if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
-
 		}
 
 		System.out.println("== 프로그램 끝 ==");
