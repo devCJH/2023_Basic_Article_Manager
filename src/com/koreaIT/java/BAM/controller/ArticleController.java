8c1ab62f049014cf15c12e2510502ce7fee1e473
devCJH <http-@naver.com>
2023-02-24 14:43:56 +0900

    게시물 작성시 회원번호 저장. article list, article detail 회원번호 출력

--- src/com/koreaIT/java/BAM/controller/ArticleController.java
+++ src/com/koreaIT/java/BAM/controller/ArticleController.java
@@ -27,6 +27,10 @@ public class ArticleController extends Controller {
 		
 		switch(methodName) {
 		case "write":
+			if (isLogined() == false) {
+				System.out.println("로그인 후 이용해주세요");
+				break;
+			}
 			doWrite();
 			break;
 		case "list":
@@ -48,11 +52,6 @@ public class ArticleController extends Controller {
 	}
 	
 	private void doWrite() {
-		if (isLogined() == false) {
-			System.out.println("로그인 후 이용해주세요");
-			return;
-		}
-		
 		int id = lastArticleId + 1;
 		lastArticleId = id;
 		String regDate = Util.getDate();
@@ -61,10 +60,10 @@ public class ArticleController extends Controller {
 		System.out.printf("내용 : ");
 		String body = sc.nextLine();
 
-		Article article = new Article(id, regDate, title, body);
+		Article article = new Article(id, regDate, loginedMember.id, title, body);
 
 		articles.add(article);
-
+		
 		System.out.printf("%d번 글이 생성되었습니다\n", id);
 	}
 
@@ -94,10 +93,10 @@ public class ArticleController extends Controller {
 			}
 		}
 
-		System.out.println("번호	|	제목	|		날짜		|	조회");
+		System.out.println("번호	|	제목	|		날짜		|	작성자	|	조회");
 		Collections.reverse(printArticles);
 		for (Article article : printArticles) {
-			System.out.printf("%d	|	%s	|	%s	|	%d\n", article.id, article.title, article.regDate,
+			System.out.printf("%d	|	%s	|	%s	|	%d	|	%d\n", article.id, article.title, article.regDate, article.memberId,
 					article.viewCnt);
 		}
 	}
@@ -123,6 +122,7 @@ public class ArticleController extends Controller {
 
 		System.out.printf("번호 : %d\n", foundArticle.id);
 		System.out.printf("날짜 : %s\n", foundArticle.regDate);
+		System.out.printf("작성자 : %d\n", foundArticle.memberId);
 		System.out.printf("제목 : %s\n", foundArticle.title);
 		System.out.printf("내용 : %s\n", foundArticle.body);
 		System.out.printf("조회수 : %d\n", foundArticle.viewCnt);
@@ -191,8 +191,8 @@ public class ArticleController extends Controller {
 	
 	public void makeTestData() {
 		System.out.println("게시물 테스트 데이터를 생성합니다");
-		articles.add(new Article(1, Util.getDate(), "제목1", "내용1", 10));
-		articles.add(new Article(2, Util.getDate(), "제목2", "내용2", 20));
-		articles.add(new Article(3, Util.getDate(), "제목3", "내용3", 30));
+		articles.add(new Article(1, Util.getDate(), 1, "제목1", "내용1", 10));
+		articles.add(new Article(2, Util.getDate(), 2, "제목2", "내용2", 20));
+		articles.add(new Article(3, Util.getDate(), 2, "제목3", "내용3", 30));
 	}
 }
--- src/com/koreaIT/java/BAM/dto/Article.java
+++ src/com/koreaIT/java/BAM/dto/Article.java
@@ -4,14 +4,16 @@ public class Article extends Dto {
 	public String title;
 	public String body;
 	public int viewCnt;
+	public int memberId;
 	
-	public Article(int id, String regDate, String title, String body){
-		this(id, regDate, title, body, 0);
+	public Article(int id, String regDate, int memberId, String title, String body){
+		this(id, regDate, memberId, title, body, 0);
 	}
 	
-	public Article(int id, String regDate, String title, String body, int viewCnt){
+	public Article(int id, String regDate, int memberId, String title, String body, int viewCnt){
 		this.id = id;
 		this.regDate = regDate;
+		this.memberId = memberId;
 		this.title = title;
 		this.body = body;
 		this.viewCnt = viewCnt;
